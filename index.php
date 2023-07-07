40ae3ff806928fff27f3fa6fab2564b89f3e75ed
Helga Chandra Eka Adhitia <128583475+ChannSys@users.noreply.github.com>
2023-07-07 11:19:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,206 @@
+
+<!--
+───╔╗
+───║║
+╔══╣╚═╦══╦═╗╔═╗╔══╦╗─╔╦══╗
+║╔═╣╔╗║╔╗║╔╗╣╔╗╣══╣║─║║══╣
+║╚═╣║║║╔╗║║║║║║╠══║╚═╝╠══║
+╚══╩╝╚╩╝╚╩╝╚╩╝╚╩══╩═╗╔╩══╝
+──────────────────╔═╝║
+──────────────────╚══╝
+-->
+
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<!-- Primary Meta Tags -->
+	<title>ChannSys Microsite by PT. Digi Pedia Group</title>
+	<meta name="title" content="ChannSys Microsite by PT. Digi Pedia Group">
+	<meta name="description" content="Powered by PT. Digi Pedia Group">
+	<meta name="keywords" content="ChannSys, Microsite, Digi Pedia, PT. Digi Pedia, Linktree">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="English">
+	<meta name="revisit-after" content="1 days">
+	<meta name="author" content="Helga Chandra Eka Adhitia">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://digi-pedia.co.id/">
+	<meta property="og:title" content="ChannSys Microsite by PT. Digi Pedia Group">
+	<meta property="og:description" content="Powered by PT. Digi Pedia Group">
+	<meta property="og:image" content="https://digi-pedia.co.id/microsite/public/img/card/card.jpg">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://digi-pedia.co.id/">
+	<meta property="twitter:title" content="ChannSys Microsite by PT. Digi Pedia Group">
+	<meta property="twitter:description" content="Powered by PT. Digi Pedia Group">
+	<meta property="twitter:image" content="https://digi-pedia.co.id/microsite/public/img/card/card.jpg">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="public/css/style.css" />
+	<link rel="stylesheet" href="dist/output.css" />
+
+	<!-- Favicon -->
+	<link rel="apple-touch-icon" sizes="57x57" href="public/img/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="public/img/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="public/img/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="public/img/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="public/img/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="public/img/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="public/img/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="public/img/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="public/img/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="public/img/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="public/img/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="public/img/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="public/img/favicon/favicon-16x16.png">
+	<link rel="manifest" href="public/img/favicon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="public/img/favicon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
+	<!-- Feather Icon -->
+	<script src="https://unpkg.com/feather-icons"></script>
+</head>
+
+<body>
+	<div
+		class="bg-fixed min-h-screen w-screen bg-gradient-to-b from-orange-400 via-red-500 to-pink-700 flex flex-col items-center overflow-hidden">
+		<header class="text-slate-100 m-2 flex flex-col items-center mt-9">
+			<img src="public/img/chann.jpg" alt="My Photo" class="w-40 rounded-full mb-3" />
+			<h1 class="text-3xl mb-1 font-bold">ChannSys</h1>
+			<p class="text-base px-6 text-center">
+				Welcome to my various <span class="font-bold">site listings!</span>
+			</p>
+		</header>
+		<!-- Baris 1 -->
+		<main class="w-full">
+			<div class="various-site flex justify-center mb-2">
+				<ul class="flex flex-col w-full md:w-[450px] gap-4 mx-8 items-center">
+					<li class="w-full">
+						<a target="_blank" href="https://play.google.com/store/apps/details?id=id.digipedia.app"
+							class="bg-orange-100 w-full flex justify-center items-center rounded-lg p-4 px-16 text-base relative hover:scale-[1.02] hover:bg-white transition text-slate-700">
+							<img src="public/img/icon/playstore_icon.png" alt="playstore icon"
+								class="w-10 absolute left-2" />
+							<div class="text-center">This My <span class="font-bold">Digi Pedia App</span></div>
+						</a>
+					</li>
+					<li class="w-full">
+						<a target="_blank" href="https://topup.digi-pedia.co.id"
+							class="bg-orange-100 w-full flex justify-center items-center rounded-lg p-4 px-16 text-base relative hover:scale-[1.02] hover:bg-white transition text-slate-700"><img
+								src="public/img/icon/digipedia_icon.png" alt="playstore icon"
+								class="w-10 absolute left-2" />
+							<div class="text-center"><span class="font-bold">Digi Pedia:</span> Game TopUp</div>
+						</a>
+					</li>
+					<li class="w-full">
+						<a target="_blank" href="https://chann.cloud/"
+							class="bg-orange-100 w-full flex justify-center items-center rounded-lg p-4 px-16 text-base relative hover:scale-[1.02] hover:bg-white transition text-slate-700"><img
+								src="public/img/icon/digipedia_icon.png" alt="playstore icon"
+								class="w-10 absolute left-2" />
+							<div class="text-center">My <span class="font-bold">Website</span></div>
+						</a>
+					</li>
+					<li class="w-full">
+						<a target="_blank" href="https://stripe.chann.cloud"
+							class="bg-orange-100 w-full flex justify-center items-center rounded-lg p-4 px-16 text-base relative hover:scale-[1.02] hover:bg-white transition text-slate-700"><img
+								src="public/img/icon/stripe_icon.png" alt="playstore icon"
+								class="w-10 absolute left-2" />
+							<div class="text-center"><span class="font-bold">Stripe</span> Payment</div>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</main>
+		<!-- Baris 2 -->
+		<header class="text-slate-100 m-2 flex flex-col items-center mt-1">
+			<h1 class="text-slate-100 mt-6 text-xl">
+				My <span class="font-bold">Repository</span>
+			</h1>
+		</header>
+		<main class="w-full">
+			<div class="various-site flex justify-center mb-2">
+				<ul class="flex flex-col w-full md:w-[450px] gap-4 mx-8 items-center">
+					<li class="w-full">
+						<a target="_blank" href="https://documenter.getpostman.com/view/26163735/2s93JtR48b#89b1a168-ee37-4a4b-955d-1230076b7538"
+							class="bg-orange-100 w-full flex justify-center items-center rounded-lg p-4 px-16 text-base relative hover:scale-[1.02] hover:bg-white transition text-slate-700"><img
+								src="public/img/icon/postman_icon.png" alt="playstore icon"
+								class="w-10 absolute left-2" />
+							<div class="text-center"><span class="font-bold">API Doc</span> Digi Pedia</div>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</main>
+		<!-- Baris 3
+		<header class="text-slate-100 m-2 flex flex-col items-center mt-1">
+			<h1 class="text-slate-100 mt-6 text-xl">
+				My <span class="font-bold">Jobs</span>
+			</h1>
+		</header>
+		<main class="w-full">
+			<div class="various-site flex justify-center mb-2">
+				<ul class="flex flex-col w-full md:w-[450px] gap-4 mx-8 items-center">
+					<li class="w-full">
+						<a target="_blank"
+							href="https://documenter.getpostman.com/view/26163735/2s93JtR48b#89b1a168-ee37-4a4b-955d-1230076b7538"
+							class="bg-orange-100 w-full flex justify-center items-center rounded-lg p-4 px-16 text-base relative hover:scale-[1.02] hover:bg-white transition text-slate-700">
+							<img src="public/img/icon/google_drive_icon.png" alt="playstore icon"
+								class="w-10 absolute left-2" />
+							<div>This My <span class="font-bold">Job Project</span></div>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</main> -->
+
+		<div class="social-media my-4 p-4 text-center">
+			<ul class="flex flex-wrap">
+				<li>
+					<a target="_blank" href="#"
+						class="bg-orange-100 block m-1 my-2 rounded-md p-2 relative text-center w-10 h-10 hover:bg-white">
+						<img src="public/img/icon/facebook_icon.png" alt="playstore icon" class="w-full" />
+					</a>
+				</li>
+
+				<li>
+					<a target="_blank" href="#"
+						class="bg-orange-100 block m-1 my-2 rounded-md p-2 relative text-center w-10 h-10 hover:bg-white">
+						<img src="public/img/icon/twitter_icon.png" alt="playstore icon" class="w-full" />
+					</a>
+				</li>
+
+				<li>
+					<a target="_blank" href="#"
+						class="bg-orange-100 block m-1 my-2 rounded-md p-2 relative text-center w-10 h-10 hover:bg-white">
+						<img src="public/img/icon/tiktok.png" alt="playstore icon" class="w-full" />
+					</a>
+				</li>
+
+				<li>
+					<a target="_blank" href="#"
+						class="bg-orange-100 block m-1 my-2 rounded-md p-2 relative text-center w-10 h-10 hover:bg-white">
+						<img src="public/img/icon/website2.png" alt="playstore icon" class="w-full" />
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div class="mb-5 text-center">
+			<p class="text-base text-white px-6">
+				Powered by <span class="font-bold"><a target="_blank" href="https://digi-pedia.co.id">PT. Digi Pedia
+						Group</a></span>
+			</p>
+		</div>
+	</div>
+</body>
+
+</html>
